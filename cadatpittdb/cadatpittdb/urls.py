fd0851bb2557b4eb9812fb74d2b311ac183316ca
Tyrica Terry Kapral <tyt3@pitt.edu>
2023-07-17 23:17:23 -0400

    Update urls.py

--- cadatpittdb/cadatpittdb/urls.py
+++ cadatpittdb/cadatpittdb/urls.py
@@ -22,5 +22,3 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path("__reload__/", include("django_browser_reload.urls"))
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
